453fef97fa664dc4f5ec3da40b007c948c3583aa
HikaLamu <lamuhika@gmail.com>
2023-06-05 16:25:14 -0400

    First message

--- Test2/src/t1/T3.java
+++ Test2/src/t1/T3.java
@@ -1,8 +1,36 @@
 package t1;
 
 public class T3 {
-	public static void main(String[] args) {
-		System.out.println("Hello");
+	
+	public static int temp;
+	
+	public static void main(String[] args) {	
+			// TODO Auto-generated method stub
+			// 0 1 2 3 4 5
+			int[] a = { 5, 2, 4, 3, 1, 6 };
+
+			for (int i = 0; i < a.length; i++) {
+
+				for (int j = i + 1; j < a.length; j++) {
+
+					if (a[i] < a[j]) {
+
+						temp = a[i];
+						a[i] = a[j];
+						a[j] = temp;
+					}
+				}
+				System.out.print(a[i] + " ");
+			}
+
+			System.out.println();
+
+			//Arrays.sort(a);
+
+			for (int i = a.length - 1; i >= 0; i--) {
+
+				System.out.print(a[i] + " ");
+			}
 	}
 
 }
